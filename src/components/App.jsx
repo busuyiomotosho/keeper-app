957d1a621ed413bda12e72610ef00cb018e38522
Busuyi Omotosho <busuyiomotosho@yahoo.com>
2023-05-22 06:10:11 +0100

    Added noted array and props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes"
+
 
 const App = () => {
     return (<div>
         <Header />
-        <Note />
+        {notes.map(entry => (
+            <Note 
+                key= {entry.id}
+                title = {entry.title}
+                content = {entry.content}
+            />
+        ))}
+        
         <Footer />
     </div>);
 }
